ce22450db0fbc4bf92f35cd1c41a508c7304f27c
Adarsh Dhanware <adarshdhanware@gmail.com>
2025-12-18 13:15:14 +0530

    AnalyzePage

--- src/app/analyze/[username]/page.tsx
+++ src/app/analyze/[username]/page.tsx
@@ -5,7 +5,15 @@ import AnalyzeDashboard from "@/components/dashboard/AnalyzeDashboard";
 
 export default function AnalyzePage() {
   const params = useParams();
-  const username = params?.username || ""; // dynamic from URL
+  const usernameParam = params?.username;
+
+  // ensure it's a string
+  const username =
+    typeof usernameParam === "string"
+      ? usernameParam
+      : Array.isArray(usernameParam)
+      ? usernameParam[0] // take the first if array
+      : ""; // fallback
 
   return <AnalyzeDashboard username={username} />;
 }
