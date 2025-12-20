b529fd43ab84d7fecff00d28fa7a6d5641982bad
Adarsh Dhanware <adarshdhanware@gmail.com>
2025-12-21 00:16:42 +0530

    Smooth Scroll scorrection

--- src/components/SmoothScroll.tsx
+++ src/components/SmoothScroll.tsx
@@ -5,21 +5,25 @@ import { useEffect } from "react";
 
 const SmoothScroll = ({ children }) => {
   useEffect(() => {
-    const lenis = new Lenis({
-      duration: 1.2, // clean, professional smoothness
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // easeOutExpo
-      smoothWheel: true,
-      smoothTouch: false,
-      wheelMultiplier: 0.9,
-    });
+  const lenis = new Lenis({
+    duration: 1.2,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    smoothWheel: true,
+    smoothTouch: false,
+    wheelMultiplier: 0.9,
+  });
 
-    function raf(time) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    }
-
+  function raf(time) {
+    lenis.raf(time);
     requestAnimationFrame(raf);
-  }, []);
+  }
+
+  requestAnimationFrame(raf);
+
+  return () => {
+    lenis.destroy();
+  };
+}, []);
   return children;
 };
 
